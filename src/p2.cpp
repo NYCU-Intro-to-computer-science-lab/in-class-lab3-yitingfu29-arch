ccd4383b310b8584fbfc17fae60212d7f7b85a71
yitingfu29-arch <yitingfu29@gmail.com>
2025-11-25 13:38:24 +0800

    Update p2.cpp

--- src/p2.cpp
+++ src/p2.cpp
@@ -3,8 +3,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    
-    
-    return 0;
+int gcd(int a,int b){
+	if(b==0)return a;
+	return gcd(b,a%b);
 }
+int main(){
+	int x,y;
+	cin>>x>>y;
+	cout<<gcd(x,y)<<endl;
+	return 0;
+}
+    
